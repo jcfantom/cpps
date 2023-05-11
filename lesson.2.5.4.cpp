7845b94c224fe2a06a1f0028158aba7263de9513
alex <naranov@yandex.ru>
2023-05-11 22:16:49 +0300

    uc-110520232216

---
+++ lesson.2.5.4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int vvod;
+    int _const = 1;
+    cin >> vvod;
+
+    while (_const <= vvod)
+    {
+        /* code */
+        cout << _const << " ";
+        _const *= 10;
+
+    }
+    
+
+    return 0;
+}
