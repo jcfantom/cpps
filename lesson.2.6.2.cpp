1e14f74fb1b6bc1df4c73c63af70a86ce5751c61
lagan-tsrb <lagan-tsrb@yandex.ru>
2023-05-12 15:44:09 +0300

    uc-120520231544

---
+++ lesson.2.6.2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int vvod, count;
+    cin >> vvod;
+    count = 0;
+
+    while (count <= vvod)
+    {
+        /* code */
+        if (count % 2 == 0)
+        {
+            /* code */
+            cout << count << " ";
+        }
+        count++;
+        
+    }
+    
+
+    return 0;
+}
