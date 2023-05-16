e1d074531638a36c6a1e71d9accf35bc8a086e99
lagan-tsrb <lagan-tsrb@yandex.ru>
2023-05-16 10:35:35 +0300

    uc-160520231035

--- lesson.3.3.8.cpp
+++ lesson.3.3.8.cpp
@@ -3,24 +3,45 @@ using namespace std;
 
 int main()
 {
-    int sum1, sum2, perem;
+    int sum1, sum2, perem, count;
     int temp1, temp2;
-    for (int i = 100000; i < 999999; i++)
+    int s1, s2;
+    count = 0;
+    for (int i = 100000; i <= 999999; i++)
     {
         /* code */
         sum1 = 0;
         sum2 = 0;
         
         perem = i;
-        while (perem >=1000)
+        temp1 = perem % 1000;
+        temp2 = perem / 1000;
+        s1 = 0;
+        s2 = 0;
+        while (temp1 != 0)
         {
             /* code */
-            temp1 = perem %10;
-            temp2 = perem / 10;
+            s1 += temp1 % 10;
+            temp1 /= 10;
+        }
+        while (temp2 != 0)
+        {
+            /* code */
+            s2 += temp2 % 10;
+            temp2 /= 10;
+        }
 
+        if (s1 == s2)
+        {
+            /* code */
+            count++; 
         }
         
+        
+        
+
+        
     }
-    
+    cout << count << endl;
     return 0;
 }
